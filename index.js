9bc84b2ac11cabe50cae97c382d82e2dbe5a2dc4
Andrew Quarfot <andrew.quarfot@gmail.com>
2022-05-20 10:04:48 -0600

    commit tree 597ea7f11b985de00edfa1fc2aaf25000b96bb97

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const removeMain = document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Andrew is the champion";
